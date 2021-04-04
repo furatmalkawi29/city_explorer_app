4e5176d367e0a4cfb6da864911a691a59bb152ec
furatmalkawi29 <malkawifurat29@gmail.com>
2021-04-04 17:49:50 +0300

    done weather and location

---
+++ server.js
@@ -0,0 +1,132 @@
+
+'use strict'; //remember read.me
+
+const express = require ('express');
+
+const server = express();
+
+require('dotenv').config(); //before PORT //to define env
+
+const PORT = process.env.PORT || 3000;
+
+
+
+const cors = require('cors');
+
+server.use(cors());
+
+//------------------------
+
+server.get('/', (req,res) => {
+
+  res.send('this is your home rout!');
+
+  console.log('this is your home rout!');
+});
+
+//-------------------------------
+
+server.get('/location', (req,res) => {
+
+  // you only have one response for each request (one respone in get function)
+  // res.send('location!');
+
+  // load data from json file to variable (like ajax)
+  let locationData = require('./data/location.json');
+
+  let newLocation = new Location (locationData);
+
+  res.send(newLocation);
+
+
+});
+
+
+const Location = function (oneLocation)
+{
+  this.search_query = 'Lynnwood';
+  this.formatted_query = oneLocation[0].display_name;
+  this.latitude = oneLocation[0].lat;
+  this.longitude = oneLocation[0].lon;
+
+
+  // {
+  //   "search_query": "seattle",
+  //   "formatted_query": "Seattle, WA, USA",
+  //   "latitude": "47.606210",
+  //   "longitude": "-122.332071"
+  // }
+};
+
+
+//-------------------------------
+
+server.get('/weather', (req,res) => {
+
+  // you only have one response for each request (one respone in get function)
+  // res.send('weather!');
+
+  // load data from json file to variable (like ajax)
+  let weatherData = require('./data/weather.json');
+
+  // oneWeather = { '':'' , '':'' , ....}
+
+  //weatherData = {'data':[], 'key':'pair' , 'key':'pair'}
+  //weatherData.data = [{},{},{},.......]
+
+  // res.send( weatherData.data );
+  // console.log( weatherData.data );
+
+  weatherData.data.forEach(item => {
+    let newWeather = new Weather (item);
+  });
+
+  res.send(Weather.all);
+
+});
+
+
+const Weather = function (oneWeather)
+{
+  this.forecast = oneWeather.weather.description;
+  this.time = new Date(oneWeather.valid_date).toDateString();
+
+  Weather.all.push(this);
+
+
+  // response should look like:
+  // [
+  //   {
+  //     "forecast": "Partly cloudy until afternoon.",
+  //     "time": "Mon Jan 01 2001"
+  //   },
+  //   {
+  //     "forecast": "Mostly cloudy in the morning.",
+  //     "time": "Tue Jan 02 2001"
+  //   },
+  //   ...
+  // ]
+};
+
+Weather.all = [];
+
+
+server.get('*',(req,res)=>{
+  // res.status(404).send('wrong route')
+  // {
+  //     status: 500,
+  //     responseText: "Sorry, something went wrong",
+  //     ...
+  //   }
+  let errObj = {
+    status: 500,
+    responseText: 'Sorry, something went wrong'
+  };
+  res.status(500).send(errObj);
+});
+
+//--------------------
+
+server.listen(PORT , () => {
+  console.log('Listning...');
+});
