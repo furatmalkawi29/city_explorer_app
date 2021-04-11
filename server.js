718ef437b05d3db41232efba2659e8afb69e5686
furatmalkawi29 <malkawifurat29@gmail.com>
2021-04-12 02:38:56 +0300

    yelp and movieDB

--- server.js
+++ server.js
@@ -2,42 +2,49 @@
 'use strict';
 
 
-//NOTE: all //-------------lab03 comments : DATATBASE SETUP
+//NOTE: //-------------lab03 comments : are for DATATBASE SETUP code
 
 //DOTENV (read our enviroment variable)
 require('dotenv').config();
 
 
-// Application Dependencies
-const express = require ('express');
+// Application Dependencies -------------------------------------------------------------:
+const express = require ('express'); // npm i express
 
 //CORS = Cross Origin Resource Sharing
-const cors = require('cors');
+const cors = require('cors'); // npm i cors
 
 //Postges
 const pg = require('pg'); //--------------lab03
-// npm i pg  //-------------lab03
+// npm i pg
+
 
 // client-side HTTP request library
 const superagent = require('superagent'); // nmp i superagent
 
-// Application Setup
+//yelp -----
+const yelp = require('yelp-fusion'); // nmp i yelp-fusion
+
+
+// Application Setup --------------------------------------------------------------------:
 const PORT = process.env.PORT || 3000;
 const server = express();
 server.use(cors());
 
-//to turn express server into pg client
-// DATABASE_URL used to make express reach spicific
-// database with this url
-// DATABASE_URL should be writtin exactly like that
+//--------- to turn express server into pg client :
+// DATABASE_URL used to make express reach spicific database with this url
+// DATABASE_URL should be named exactly like that
 // to mach heroku
 //DATABASE_URL=postgresql://furatmalkawi:12345@localhost:5432/class8
-//localhost:5432/class8 --> 5432 reserved port for postgress local server
+//localhost:5432 --> 5432 reserved port for postgress local server
 //class8 --> name of database
 // env: environment variable
-// const client = new pg.Client(process.env.DATABASE_URL); //-------------lab03
 
-const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false}}); //-------------lab03
+// to run locally:
+const client = new pg.Client(process.env.DATABASE_URL); //-------------lab03
+
+//to run on heroku:
+// const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false}}); //-------------lab03
 
 
 // Routes
@@ -45,10 +52,12 @@ server.get('/', homeRoutHandler );
 server.get('/location', locationRoutHandler);
 server.get('/weather', weatherRoutHandler);
 server.get('/parks', parkRoutHandler);
+server.get('/movies', movieRoutHandler);
+server.get('/yelp', yelpRoutHandler);
 server.get('*', allRoutHandler);
 
 
-//Functions-------------------------------------------
+//Functions---------------------------------------------------------------
 function homeRoutHandler (req,res) {
 
   res.send('this is your home rout!');
@@ -62,20 +71,25 @@ function locationRoutHandler (req,res) {
 
   let cityName = req.query.city; //'amman'
 
+  // API data
+  let key = process.env.LOCATION_KEY;
+  let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
 
   // check if i have 'amman' in data base?
-  checkDatabase(res,cityName);
+  checkDatabase(res,cityName,LocURL);
 }
 
 //--------------------------------
 
-function checkDatabase (res,cityName)
+function checkDatabase (res,cityName,URL)
 {
   console.log(`/////////////////////////////NEW City Explore//////////////////////////////////`);
   console.log(`checking ${cityName} in database Table...`);
 
   // results.rows --> array of table data records
   // [{},{},....]
+
+  //result ---> {%$$$$%@ ...unneccessary data .. rows: [{},{}] }
   let SQL = `SELECT search_query FROM cities;`;
   client.query(SQL)
     .then (result=>{
@@ -101,45 +115,48 @@ function checkDatabase (res,cityName)
             console.log(`Returned ${cityName} data from database is :`);
             console.log(result.rows);
 
-            res.send(result.rows[0]); // {}
+            res.send(result.rows[0]); // result.rows[0]= { first obj (record)}
             console.log(`Response sent! :--------------------`);
             console.log(result.rows[0]);
-
           });
 
 
       }
       else {
-        console.log(`--------------------------------------`);
-        console.log(` ${cityName} not found in database!`);
 
-        //1) Hit API to get city DATA :
-        let key = process.env.LOCATION_KEY;
-        let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
-
-        superagent.get(LocURL) //send request to LocationIQ API
-          .then(fullLocationData => {
-            let locationData = fullLocationData.body;
-            const locationObj = new Location(cityName, locationData);
-
-
-
-            //2) Save city Api DATA in databas :
-
-            console.log(`--------------------------------------`);
-            console.log(` This is ${cityName} api data:`);
-
-            console.log(locationObj);
-
-
-            saveToDatabaseApi(res,locationObj);
-
-          }).catch(error=>{
-            res.send(error);
-          });//speragent
-      }//else
+        hitApi(res,cityName,URL);}
     });
 }//function
+
+
+function hitApi (res,cityName,URL)
+{
+  console.log(`--------------------------------------`);
+  console.log(` ${cityName} not found in database!`);
+
+  //1) Hit API to get city DATA :
+  superagent.get(URL) //send request to LocationIQ API
+    .then(fullLocationData => {
+      let locationData = fullLocationData.body;
+      const locationObj = new Location(cityName, locationData);
+
+
+
+      //2) Save city Api DATA in databas :
+
+      console.log(`--------------------------------------`);
+      console.log(` This is ${cityName} api data:`);
+
+      console.log(locationObj);
+
+
+      saveToDatabaseApi(res,locationObj);
+
+    }).catch(error=>{
+      res.send(error);
+    });//speragent
+}//else
+
 //-----------------------------
 function saveToDatabaseApi (res,locationObj)
 {
@@ -260,6 +277,99 @@ function parkRoutHandler (req,res) {
 }
 //--------------------------
 
+function movieRoutHandler (req,res) {
+
+  let key = process.env.MOVIE_API_KEY;
+
+
+  let cityName = req.query.search_query;
+
+  // test if api request is working:
+  // in browser : https://api.themoviedb.org/3/search/movie?api_key=afbde666843f76b1afa9e080eca93939&query=paris
+
+  // api returns : body data
+
+  //superagent : body data + rubish data
+  let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
+
+  superagent.get(movieURL) //send request to MovieDB API
+    .then(fullMovieData => {
+
+      let movieData = fullMovieData.body.results; //body {"":"" , "":"" , "results":[{},{}]}
+      // console.log(movieData);
+
+      let movieObjArr = movieData.map(item => {
+        return new Movie (item); });
+
+      res.send(movieObjArr);
+
+
+    }).catch(error=>{
+      res.send(error);
+    });
+}
+
+//---------------------------------------
+//yelp website :
+// gives code samples on github :
+// https://github.com/Yelp/yelp-fusion#code-samples
+
+function yelpRoutHandler (req,res)
+{
+  let key = process.env.YELP_API_KEY;
+
+  const client = yelp.client(key);
+
+  //--------------------------Pagination :
+
+  let page = req.query.page;
+  //each click on 'load more' sends new request
+  //page is query string parameter
+  //second click --> page = 2
+  //5th click --> page = 5
+
+  let itemsPerPage = 5;
+  let startIndex = ((page - 1) * itemsPerPage + 1);
+
+
+  // choice 1 ------------------------:
+  let cityName = req.query.search_query;
+  let strQueryObj ={location: cityName,
+    limit: itemsPerPage,
+    offset: startIndex};
+
+  // or choice 2 -----------------------:
+  // let lat=	req.query.latitude;
+  // let lon = req.query.longitude;
+  // let strQueryObj ={latitude: lat,
+  //   longitude: lon,
+  //   limit: itemsPerPage,
+  // offset: startIndex};
+
+
+  client.search(strQueryObj) //send request to yelp Api , including key
+    .then(fullYelpData => {
+
+      // fullYelpData.body = '{...... , "businesses":[]}'
+      // body is string in yelp api
+      // parse --> fullYelpData.body = { ... , "businesses":[]}
+      let yelpData = JSON.parse(fullYelpData.body).businesses;
+
+      //another way:
+      // let yelpData = fullYelpData.jsonBody.businesses; //jsonBody is not string = {...."businesses":[]}
+
+      let yelpObjArr = yelpData.map(item => {
+        return new Yelp (item); });
+
+      res.send(yelpObjArr);
+
+
+    }).catch(error=>{
+      res.send(error);
+    });
+}
+//-----------------------------------------
+
 function allRoutHandler (req,res) {
   let errObj = {
     status: 500,
@@ -279,14 +389,6 @@ const Location = function (cityName,oneLocation)
   this.formatted_query = oneLocation[0].display_name;
   this.latitude = oneLocation[0].lat;
   this.longitude = oneLocation[0].lon;
-
-
-  // {
-  //   "search_query": "seattle",
-  //   "formatted_query": "Seattle, WA, USA",
-  //   "latitude": "47.606210",
-  //   "longitude": "-122.332071"
-  // }
 };
 
 
@@ -297,20 +399,6 @@ const Weather = function (oneWeather)
 {
   this.forecast = oneWeather.weather.description;
   this.time = new Date(oneWeather.valid_date).toDateString();
-
-
-  // response should look like:
-  // [
-  //   {
-  //     "forecast": "Partly cloudy until afternoon.",
-  //     "time": "Mon Jan 01 2001"
-  //   },
-  //   {
-  //     "forecast": "Mostly cloudy in the morning.",
-  //     "time": "Tue Jan 02 2001"
-  //   },
-  //   ...
-  // ]
 };
 
 
@@ -325,20 +413,67 @@ const Park = function (onePark)
   this.fee = onePark.entranceFees[0].cost;
   this.description = onePark.description;
   this.url = onePark.url;
-  // response should look like:
-
-  // [
-  //   {
-  //    "name": "Klondike Gold Rush - Seattle Unit National Historical Park",
-  //    "address": "319 Second Ave S., Seattle, WA 98104",
-  //    "fee": "0.00",
-  //    "description": "Seattle flourished during and after the Klondike Gold Rush. Merchants supplied people from around the world passing through this port city on their way to a remarkable adventure in Alaska. Today, the park is your gateway to learn about the Klondike Gold Rush, explore the area's public lands, and engage with the local community.",
-  //    "url": "https://www.nps.gov/klse/index.htm"
-  //   },
-  // ]
 };
 
 
+//------------------------------------
+let Movie = function (oneMovie)
+{
+
+  this.title = oneMovie.title ,
+  this.overview = oneMovie.overview,
+  this.average_votes = oneMovie.average_votes ,
+  this.total_votes = oneMovie.vote_count,
+  //---------------------NOTES:
+  //from Api documentation --> configration :
+  //https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+
+  //base url: https://image.tmdb.org/t/p
+  //image size:/w500
+  //poster_path: 8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+
+  //this.image_url = oneMovie.poster_path,
+  //you get : "image_url": "/3LIn7bskDwsmyxMWTNWRIPSCm2X.jpg"
+  this.image_url = `https://image.tmdb.org/t/p/w500${oneMovie.poster_path}`,
+  this.popularity = oneMovie.popularity ,
+  this.released_on = oneMovie.release_date;};
+
+// {
+//   "title": "Love Happens",
+//   "overview": "Dr. Burke Ryan is a successful self-help author and motivational speaker with a secret. While he helps thousands of people cope with tragedy and personal loss, he secretly is unable to overcome the death of his late wife. It's not until Burke meets a fiercely independent florist named Eloise that he is forced to face his past and overcome his demons.",
+//   "average_votes": "5.80",
+//   "total_votes": "282",
+//   "image_url": "https://image.tmdb.org/t/p/w500/pN51u0l8oSEsxAYiHUzzbMrMXH7.jpg",
+//   "popularity": "15.7500",
+//   "released_on": "2009-09-18"
+// },;
+
+//------------------------------
+
+const Yelp = function (oneYelp)
+{
+
+  this.name = oneYelp.name,
+  this.image_url = oneYelp.image_url ,
+  this.price = oneYelp.price,
+  this.rating = oneYelp.rating,
+  this.url = oneYelp.url;
+
+
+  // [
+  //   {
+  //     "name": "Pike Place Chowder",
+  //     "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
+  //     "price": "$$   ",
+  //     "rating": "4.5",
+  //     "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
+  //   }, ...
+  //  ]
+
+};
+
+
+//----------------------------------------
 //Server Listner
 
 // used to debug --> how?
